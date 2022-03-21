a3db5609c33343846d0809693bc9d33e5dcf2748
Carlos <krlos173173@gmail.com>
2022-03-20 22:54:09 -0300

    Se agrega funcion get spiner selection

--- TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/ValeActivity.kt
+++ TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/ValeActivity.kt
@@ -2,34 +2,79 @@ package com.mana.transportesruta7app
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatSpinner
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_vale.*
 
+
 // Prueba commit
 class ValeActivity : AppCompatActivity() {
     val db = Firebase.firestore
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
+        var empresa = ""
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_vale)
 
-        //primero
-        //setup()
+        crearValeButton.setOnClickListener() {
+            crearVale()
+        }
+
+        setup()
+
     }
-
     private fun setup() {
-
-        //Segundo
         cargarCenctrodeCostos()
         cargarDirecciones()
         cargarEmpresas()
+        crearVale()
+
 
     }
-        //Tercero
+
+    fun crearVale() {
+
+            val posicion = spnEmpresas.selectedItemPosition
+
+
+            val vale = hashMapOf(
+                "Fecha" to "20-03-2022",
+                "Chofer" to "German Satula",
+                "Movil" to "3",
+                "Patente" to "XX00XX",
+                "Empresa" to spnEmpresas.getItemAtPosition(posicion),
+                "Centro de costo" to "Soprole",
+                "Hora Inicio" to "13:00",
+                "Hora Termino" to "13:30",
+                "Recorrido" to "Las padredas - chochonco city",
+                "Cliente" to "Rosalia",
+                "Rut" to "18.465.654-9",
+                "Firma" to "Si"
+            )
+            db.collection("Vales").document("test")
+                .set(vale)
+                .addOnSuccessListener {
+                    Toast.makeText(
+                        applicationContext,
+                        "Funciono",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(
+                        applicationContext,
+                        "No Funciono",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+    }
     fun cargarCenctrodeCostos(){
         val docRef = db.collection("Listas").document("CC")
         docRef.get().addOnSuccessListener { document ->
@@ -103,4 +148,10 @@ class ValeActivity : AppCompatActivity() {
             Log.d("TAG", "get failed with ", exception)
         }
     }
+
+
 }
+
+
+
+
