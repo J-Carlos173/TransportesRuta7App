76e9e517583c27635a2f40b973fb02a8851af11e
filopolfelix <felipfuentealbaci@gmail.com>
2022-03-21 23:59:53 -0300

    agregar pasajero y vista para canvas firma

---
+++ TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/PasajeroActivity.kt
@@ -0,0 +1,11 @@
+package com.mana.transportesruta7app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PasajeroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pasajero)
+    }
+}
