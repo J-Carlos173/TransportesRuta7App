88548f73a7af0ac51cf2984d9121fd07462b5135
Carlos <krlos173173@gmail.com>
2022-04-01 01:14:24 -0300

    Se actualiza ValeActivity

--- TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/ValeActivity.kt
+++ TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/ValeActivity.kt
@@ -6,32 +6,32 @@ import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_vale.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
-// Prueba commit
 class ValeActivity : AppCompatActivity() {
-    val db = Firebase.firestore
-    //val mEmail = intent.getStringExtra("mEmail").toString()
 
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vale)
-
         crearValeButton.setOnClickListener() {
             crearVale()
         }
-
         setup()
 
-        var msg = intent.getStringExtra("mensaje").toString()
-        Toast.makeText(applicationContext,msg,Toast.LENGTH_SHORT).show()
     }
     override fun onBackPressed() {
+        val mEmail = intent.getStringExtra("mEmail").toString()
         val intent = Intent(this, HomeActivity::class.java)
-        //intent.putExtra("mEmail", mEmail)
+        intent.putExtra("mEmail", mEmail)
         this.startActivity(intent)
     }
 
@@ -39,24 +39,30 @@ class ValeActivity : AppCompatActivity() {
         cargarCenctrodeCostos()
         cargarDirecciones()
         cargarEmpresas()
-        crearVale()
+        cargarDatos()
+
+        crearValeButton.setOnClickListener(){
+            crearVale()
+        }
     }
 
     fun crearVale() {
+        val mEmail =intent.getStringExtra("mEmail").toString()
         val vale = hashMapOf(
+                "Email"             to mEmail,
                 "Fecha"             to fechaTextView.text.toString(),
+                "Hora Inicio"       to "10:30",
+                "Hora Fin"          to "11:00",
                 "Chofer"            to choferTextView.text.toString(),
                 "Movil"             to movilTextView.text.toString(),
                 "Patente"           to patenteTextView.text.toString(),
-                "Hora Inicio"       to horaTextView.text.toString(),
-                "Hora Termino"      to horaTextView.text.toString(),
                 "Empresa"           to spnEmpresas.getItemAtPosition(spnEmpresas.selectedItemPosition),
                 "Centro de costo"   to spnCentroCosto.getItemAtPosition(spnCentroCosto.selectedItemPosition),
-                "Recorrido"         to spnDirecciones.getItemAtPosition(spnDirecciones.selectedItemPosition),
-                "Email"             to "krlos173@hotmail.com",
-                "Cliente"           to "Rosalia",
-                "Rut"               to "18.465.654-9",
-                "Firma"             to "Si"
+                "Direccion Inicio"  to spnDireccionInicio.getItemAtPosition(spnDireccionInicio.selectedItemPosition),
+                "Direccion Final"   to spnDireccionFin.getItemAtPosition(spnDireccionFin.selectedItemPosition),
+                "Cliente"           to pasajeroEditText.text.toString(),
+                "Rut"               to rutEditText.text.toString(),
+                "Firma"             to "Firma.png"
         )
 
         val contador = db.collection("Vales")
@@ -64,7 +70,10 @@ class ValeActivity : AppCompatActivity() {
         val size = documentSnapshots.size() + 1
 
         db.collection("Vales").document(size.toString()).set(vale).addOnSuccessListener {
-            Toast.makeText(applicationContext, "Funciono", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "Vale Creado", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("mEmail", mEmail)
+            this.startActivity(intent)
         }.addOnFailureListener {
             Toast.makeText(applicationContext, "No Funciono", Toast.LENGTH_SHORT).show()
         }
@@ -133,8 +142,8 @@ class ValeActivity : AppCompatActivity() {
                     list.add(cCosto.substring(found + 1))
                 }
                 val adapter = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list)
-                spnDirecciones.adapter = adapter
-                spnDirecciones2.adapter = adapter
+                spnDireccionInicio.adapter = adapter
+                spnDireccionFin.adapter = adapter
             }
             else {
                 Log.d("TAG", "No such document")
@@ -143,9 +152,37 @@ class ValeActivity : AppCompatActivity() {
             Log.d("TAG", "get failed with ", exception)
         }
     }
+    fun cargarDatos(){
+        val mEmail =intent.getStringExtra("mEmail").toString()
+
+        val sdf = SimpleDateFormat("dd/M/yyyy")
+        val currentDate = sdf.format(Date())
+        val docRef = db.collection("Personas").document(mEmail)
+        docRef.get().addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("TAG", "${document.id} => ${document.data}")
+
+                    choferTextView.text     = document.data?.getValue("Nombres").toString()
+                    movilTextView.text      = document.data?.getValue("Movil").toString()
+                    patenteTextView.text    = document.data?.getValue("Patente").toString()
+                    fechaTextView.text      = currentDate.toString()
+                    Log.d("TAG", "******************Todo Correcto******************")
+                    Toast.makeText(this, "Se han cargado los datos", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.d("TAG", "El documento tiene un error document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("TAG", "No se encontro el Documento", exception)
+            }
 
 
+
+    }
 }
+
+
+
 
 
 
