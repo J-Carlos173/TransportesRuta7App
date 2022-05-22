b43b1b24a32af45468a69cef6f30c7f670b351db
Carlos <krlos173173@gmail.com>
2022-05-22 00:44:04 -0400

    actualizacion

--- TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/AgregarRutaActivity.kt
+++ TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/AgregarRutaActivity.kt
@@ -1,19 +1,283 @@
 package com.mana.transportesruta7app
 
 import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_agregar_ruta.*
+import kotlinx.android.synthetic.main.activity_vale_direcciones.*
+
 
 class AgregarRutaActivity : AppCompatActivity() {
+
+
+    companion object {
+        private const val ZOOM_SIZE = 14f
+        private const val POLYLINE_WIDTH = 12f
+    }
+    private var lat1 :Double? = 0.0
+    private var long1 :Double? = 0.0
+    private var lat2 :Double? = 0.0
+    private var long2 :Double? = 0.0
+
+
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_ruta)
+
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+
+        setup(email ?: "", provider ?: "")
+
+
+
+        // Autocompletado y mapa
+        // Fetching API_KEY which we wrapped
+        val ai: ApplicationInfo = applicationContext.packageManager
+            .getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
+        val value = ai.metaData["api_key"]
+        val apiKey = value.toString()
+
+        // Initializing the Places API with the help of our API_KEY
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, apiKey)
+        }
+
+        // Initialize Autocomplete Fragments from the main activity layout file
+        val autocompleteSupportFragment1 =
+            supportFragmentManager.findFragmentById(R.id.rutaInicioText) as AutocompleteSupportFragment?
+        val autocompleteSupportFragment2 =
+            supportFragmentManager.findFragmentById(R.id.rutaFinText) as AutocompleteSupportFragment?
+
+        // Information that we wish to fetch after typing the location and clicking on one of the options
+        autocompleteSupportFragment1!!.setPlaceFields(
+            listOf(
+
+                Place.Field.NAME,
+                Place.Field.ADDRESS,
+                Place.Field.PHONE_NUMBER,
+                Place.Field.LAT_LNG,
+                Place.Field.OPENING_HOURS,
+                Place.Field.RATING,
+                Place.Field.USER_RATINGS_TOTAL
+
+            )
+        )
+
+        autocompleteSupportFragment2!!.setPlaceFields(
+            listOf(
+
+                Place.Field.NAME,
+                Place.Field.ADDRESS,
+                Place.Field.PHONE_NUMBER,
+                Place.Field.LAT_LNG,
+                Place.Field.OPENING_HOURS,
+                Place.Field.RATING,
+                Place.Field.USER_RATINGS_TOTAL
+
+            )
+        )
+
+        // Display the fetched information after clicking on one of the options
+        autocompleteSupportFragment1.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+
+                // Text view where we will append the information that we fetch
+
+                // Information about the place
+                val name = place.name
+                val address = place.address
+                //val phone = place.phoneNumber.toString()
+                val latlng = place.latLng
+                val latitude1 = latlng?.latitude
+                val longitude1 = latlng?.longitude
+                lat1 = latitude1
+                long1 = longitude1
+
+                val isOpenStatus : String = if(place.isOpen == true){
+                    "Open"
+                } else {
+                    "Closed"
+                }
+
+                val rating = place.rating
+                val userRatings = place.userRatingsTotal
+
+
+
+                /*textView.text = "Name: $name \nAddress: $address \nPhone Number:  \n" +
+                        "Latitude, Longitude: $latitude , $longitude \nIs open: $isOpenStatus \n" +
+                        "Rating: $rating \nUser ratings: $userRatings"*/
+            }
+
+            override fun onError(status: Status) {
+                Toast.makeText(applicationContext,"Some error occurred", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        // Display the fetched information after clicking on one of the options
+        autocompleteSupportFragment2.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+
+                // Text view where we will append the information that we fetch
+
+
+                // Information about the place
+                val name = place.name
+                val address = place.address
+                //val phone = place.phoneNumber.toString()
+                val latlng = place.latLng
+                var latitude2 = latlng?.latitude
+                var longitude2 = latlng?.longitude
+                lat2 = latitude2
+                long2 = longitude2
+
+                val isOpenStatus : String = if(place.isOpen == true){
+                    "Open"
+                } else {
+                    "Closed"
+                }
+
+                val rating = place.rating
+                val userRatings = place.userRatingsTotal
+
+
+
+                /*textView.text = "Name: $name \nAddress: $address \nPhone Number:  \n" +
+                        "Latitude, Longitude: $latitude , $longitude \nIs open: $isOpenStatus \n" +
+                        "Rating: $rating \nUser ratings: $userRatings"*/
+            }
+            override fun onError(status: Status) {
+                Toast.makeText(applicationContext,"Some error occurred", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+    private fun cargarCenctrodeCostos(){
+        val docRef = db.collection("Listas").document("CC")
+        docRef.get().addOnSuccessListener { document ->
+            if (document != null) {
+                var listaCC = document.data.toString()
+                listaCC = listaCC.replace("{", "")
+                listaCC = listaCC.replace("}", "")
+                val arr = listaCC.split(",")
+                println(arr)
+                val list : MutableList<String> = ArrayList()
+                for (cCosto in arr) {
+                    val found = cCosto.indexOf("=");
+                    list.add(cCosto.substring(found + 1))
+                }
+                val adapter = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list)
+                spnCentroCosto.adapter = adapter
+            }
+            else {
+                Log.d("TAG", "No such document")
+            }
+        }.addOnFailureListener { exception ->
+            Log.d("TAG", "get failed with ", exception)
+        }
+    }
+    private fun cargarEmpresas(){
+        val docRef = db.collection("Listas").document("Empresas")
+        docRef.get().addOnSuccessListener { document ->
+            if (document != null) {
+                var listaCC = document.data.toString()
+                listaCC = listaCC.replace("{", "")
+                listaCC = listaCC.replace("}", "")
+                val arr = listaCC.split(",")
+                println(arr)
+                val list : MutableList<String> = ArrayList()
+                for (cCosto in arr) {
+                    val found = cCosto.indexOf("=");
+                    list.add(cCosto.substring(found + 1))
+                }
+                val adapter = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list)
+                spnEmpresas.adapter = adapter
+            }
+            else {
+                Log.d("TAG", "No such document")
+            }
+        }.addOnFailureListener { exception ->
+            Log.d("TAG", "get failed with ", exception)
+        }
+    }
+    private fun setup(email: String, provider: String) {
+        cargarCenctrodeCostos()
+
+        cargarEmpresas()
+        AgregarRutaButton.setOnClickListener {
+            val db = FirebaseFirestore.getInstance()
+            val rutaEmpresa = hashMapOf(
+                "ruta_nombre"       to rutaText.text.toString(),
+                "ruta_inicio"       to "["+lat1.toString()+","+long1.toString()+"]",
+                "ruta_fin"          to "["+lat2.toString()+","+long2.toString()+"]",
+                "ruta_empresa"      to spnEmpresas.getItemAtPosition(spnEmpresas.selectedItemPosition),
+                "ruta_centrocosto"  to spnCentroCosto.getItemAtPosition(spnCentroCosto.selectedItemPosition)
+            )
+
+                db.collection("RutasEmpresa").document(rutaText.text.toString()).set(rutaEmpresa).addOnSuccessListener {
+                    showFelicidades()
+                    showAdmin(email, provider)
+                }.addOnFailureListener {
+                    showError()
+                }
+
+            showFelicidades()
+        }
     }
 
+
     override fun onBackPressed() {
-        val mEmail = intent.getStringExtra("mEmail").toString()
-        val intent = Intent(this, HomeActivity::class.java)
-        intent.putExtra("mEmail", mEmail)
-        this.startActivity(intent)
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        showAdmin(email.toString(), provider.toString())
+
+    }
+
+    private fun showAdmin(email: String, provider: String) {
+        val adminIntent = Intent(this,AdminActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider)
+        }
+        startActivity(adminIntent)
+    }
+    private fun showFelicidades() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Felicidades")
+        builder.setMessage("Se a agregado a la lista")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun showError() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Ocurrio un problema")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
--- TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/CrearValeEmpresaActivity.kt
+++ TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/CrearValeEmpresaActivity.kt
@@ -1,24 +1,67 @@
 package com.mana.transportesruta7app
 
+
+import android.content.Context
 import android.content.Intent
+import android.graphics.*
 import android.os.Bundle
+import android.os.Environment
+import android.util.AttributeSet
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_crear_vale_empresa.*
-import kotlinx.android.synthetic.main.activity_vale_direcciones.*
+import java.io.File
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
 
 
+class CrearValeEmpresaActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
 
-class CrearValeEmpresaActivity : AppCompatActivity() {
 
+    private var btnClear: Button? = null
+    private var btnSave: Button? = null
+    private var file: File? = null
+    private var canvasLL: LinearLayout? = null
+    private var view: View? = null
+    private var mSignature: CrearValeEmpresaActivity.signature? = null
+    private var bitmap: Bitmap? = null
     val db = Firebase.firestore
+
+    companion object {
+        const val STROKE_WIDTH = 5f
+        const val HALF_STROKE_WIDTH = STROKE_WIDTH / 2
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        println("item seleccionado")
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        println("nada seleccionado")
+    }
+    private fun cargarRuta(){
+
+        val spinner: Spinner = findViewById(R.id.spnRuta)
+        spinner.onItemSelectedListener = this
+        val db = FirebaseFirestore.getInstance()
+        val ruta = spnRuta.getItemAtPosition(spnRuta.selectedItemPosition)
+        val docRef = db.collection("RutasEmpresa").document(ruta.toString())
+
+    }
+
+    // Creando un Directorio aparte para guardar imagenes generadas
+    private var directory = Environment.getExternalStorageDirectory().path + "/Firma/"
+    private var pic_name = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+    private var StoredPath = "$directory$pic_name.png"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_vale_empresa)
@@ -32,15 +75,55 @@ class CrearValeEmpresaActivity : AppCompatActivity() {
             crearVale(email)
         }
 
+
+        canvasLL = findViewById<View>(R.id.canvasLL) as LinearLayout
+        mSignature = signature(applicationContext, null)
+        mSignature!!.setBackgroundColor(Color.WHITE)
+        canvasLL!!.addView(
+            mSignature,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        btnClear = findViewById<View>(R.id.btnclear) as Button
+        btnSave = findViewById<View>(R.id.btnsave) as Button
+        view = canvasLL
+        btnClear!!.setOnClickListener { mSignature!!.clear() }
+        btnSave!!.setOnClickListener {
+            view!!.isDrawingCacheEnabled = true
+            mSignature!!.save(view, StoredPath)
+            Toast.makeText(applicationContext, "Guardado con exito", Toast.LENGTH_SHORT).show()
+        }
+
+        // Metodo para crear Directorio , Si el Directorio no exixte
+        file = File(directory)
+        if (!file!!.exists()) {
+            file!!.mkdir()
+        }
+    }
+    private fun setup(email: String) {
+        //cargarCenctrodeCostos()
+        //cargarEmpresas()
+        cargarDirecciones()
+
+        cargarDatos(email)
+        cargarSpnRutas()
+        cargarRuta()
+
     }
 
-    private fun setup(email: String) {
-        cargarCenctrodeCostos()
-        cargarDirecciones()
-        cargarEmpresas()
-        cargarDatos(email)
-
-
+    private fun cargarSpnRutas(){
+        val list : MutableList<String> = ArrayList()
+        db.collection("RutasEmpresa").get().addOnSuccessListener { documents ->
+                for (document in documents) {
+                    list.add(document.id)
+                }
+            val adapter = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list)
+            spnRuta.adapter = adapter
+            }
+            .addOnFailureListener { exception ->
+                Log.w("TAG", "Error getting documents: ", exception)
+            }
+        // [END get_m
     }
     private fun crearVale(email: String) {
 
@@ -57,10 +140,10 @@ class CrearValeEmpresaActivity : AppCompatActivity() {
                 "vale_Rut"              to rutValeEditText.text.toString(),
                 "vale_Patente"          to patenteEditText.text.toString(),
                 "vale_Movil"            to nroMovilEditText.text.toString(),
-                "vale_Empresa"          to spnEmpresas.getItemAtPosition(spnEmpresas.selectedItemPosition),
-                "vale_CC"               to spnCentroCosto.getItemAtPosition(spnCentroCosto.selectedItemPosition),
-                "vale_Inicio"           to autocomplete_fragment1.toString(),
-                "vale_Final"            to autocomplete_fragment2.toString(),
+                "vale_Empresa"          to centroCostoText.text.toString(),
+                "vale_CC"               to EmpresaText.text.toString(),
+                "vale_Inicio"           to spnRuta.getItemAtPosition(spnRuta.selectedItemPosition),
+                "vale_Final"            to spnRuta.getItemAtPosition(spnRuta.selectedItemPosition),
                 "vale_cliente_nombre"   to nombreClienteText.text.toString(),
                 "vale_cliente_rut"      to rutClienteText.text.toString(),
                 "vale_Tipo"             to "Empresa",
@@ -79,13 +162,7 @@ class CrearValeEmpresaActivity : AppCompatActivity() {
         }
     }
     }
-    private fun showHome(email: String, provider: ProviderType) {
-        val homeIntent = Intent(this,HomeActivity::class.java).apply {
-            putExtra("email", email)
-            putExtra("provider", provider.name)
-        }
-        startActivity(homeIntent)
-    }
+    /*
     private fun cargarCenctrodeCostos(){
         val docRef = db.collection("Listas").document("CC")
         docRef.get().addOnSuccessListener { document ->
@@ -109,8 +186,8 @@ class CrearValeEmpresaActivity : AppCompatActivity() {
         }.addOnFailureListener { exception ->
             Log.d("TAG", "get failed with ", exception)
         }
-    }
-    private fun cargarEmpresas(){
+    }*/
+    /*private fun cargarEmpresas(){
         val docRef = db.collection("Listas").document("Empresas")
         docRef.get().addOnSuccessListener { document ->
             if (document != null) {
@@ -133,7 +210,7 @@ class CrearValeEmpresaActivity : AppCompatActivity() {
         }.addOnFailureListener { exception ->
             Log.d("TAG", "get failed with ", exception)
         }
-    }
+    }*/
     private fun cargarDirecciones(){
         val docRef = db.collection("Listas").document("Direcciones")
         docRef.get().addOnSuccessListener { document ->
@@ -188,14 +265,125 @@ class CrearValeEmpresaActivity : AppCompatActivity() {
         val bundle = intent.extras
         val email = bundle?.getString("email")
         val provider = bundle?.getString("provider")
-        showAdmin(email.toString(), provider.toString())
+        showHome(email.toString(), ProviderType.BASIC)
     }
-    private fun showAdmin(email: String, provider: String) {
-        val adminIntent = Intent(this,AdminActivity::class.java).apply {
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this,HomeActivity::class.java).apply {
             putExtra("email", email)
-            putExtra("provider", provider)
+            putExtra("provider", provider.name)
         }
-        startActivity(adminIntent)
+        startActivity(homeIntent)
+    }
+    inner class signature(context: Context?, attrs: AttributeSet?) :
+        View(context, attrs) {
+        private val paint = Paint()
+        private val path = Path()
+        private var lastTouchX = 0f
+        private var lastTouchY = 0f
+        private val dirtyRect = RectF()
+        fun save(v: View?, StoredPath: String?) {
+            Log.v("log_tag", "Width: " + v!!.width)
+            Log.v("log_tag", "Height: " + v.height)
+            if (bitmap == null) {
+                bitmap = Bitmap.createBitmap(canvasLL!!.width, canvasLL!!.height, Bitmap.Config.RGB_565)
+            }
+            val canvas = Canvas(bitmap!!)
+            try {
+                // exportar el archivo
+                val mFileOutStream = FileOutputStream(StoredPath)
+                v.draw(canvas)
+
+                // convertir el archivo exportado a .png
+                bitmap!!.compress(Bitmap.CompressFormat.PNG, 90, mFileOutStream)
+                mFileOutStream.flush()
+                mFileOutStream.close()
+            } catch (e: Exception) {
+                Log.v(" ", e.toString())
+            }
+        }
+
+        fun clear() {
+            path.reset()
+            invalidate()
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            canvas.drawPath(path, paint)
+        }
+
+        override fun onTouchEvent(event: MotionEvent): Boolean {
+            val eventX = event.x
+            val eventY = event.y
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    path.moveTo(eventX, eventY)
+                    lastTouchX = eventX
+                    lastTouchY = eventY
+                    return true
+                }
+                MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP -> {
+                    resetDirtyRect(eventX, eventY)
+                    val historySize = event.historySize
+                    var i = 0
+                    while (i < historySize) {
+                        val historicalX = event.getHistoricalX(i)
+                        val historicalY = event.getHistoricalY(i)
+                        expandDirtyRect(historicalX, historicalY)
+                        path.lineTo(historicalX, historicalY)
+                        i++
+                    }
+                    path.lineTo(eventX, eventY)
+                }
+                else -> {
+                    debug("Envento tactil ignorado: $event")
+                    return false
+                }
+            }
+            invalidate(
+                (dirtyRect.left - CrearValeEmpresaActivity.HALF_STROKE_WIDTH).toInt(),
+                (dirtyRect.top - CrearValeEmpresaActivity.HALF_STROKE_WIDTH).toInt(),
+                (dirtyRect.right + CrearValeEmpresaActivity.HALF_STROKE_WIDTH).toInt(),
+                (dirtyRect.bottom + CrearValeEmpresaActivity.HALF_STROKE_WIDTH).toInt()
+            )
+            lastTouchX = eventX
+            lastTouchY = eventY
+            return true
+        }
+
+        private fun debug(string: String) {
+            Log.v("log_tag", string)
+        }
+
+        private fun expandDirtyRect(historicalX: Float, historicalY: Float) {
+            if (historicalX < dirtyRect.left) {
+                dirtyRect.left = historicalX
+            } else if (historicalX > dirtyRect.right) {
+                dirtyRect.right = historicalX
+            }
+            if (historicalY < dirtyRect.top) {
+                dirtyRect.top = historicalY
+            } else if (historicalY > dirtyRect.bottom) {
+                dirtyRect.bottom = historicalY
+            }
+        }
+
+        private fun resetDirtyRect(eventX: Float, eventY: Float) {
+            dirtyRect.left = Math.min(lastTouchX, eventX)
+            dirtyRect.right = Math.max(lastTouchX, eventX)
+            dirtyRect.top = Math.min(lastTouchY, eventY)
+            dirtyRect.bottom = Math.max(lastTouchY, eventY)
+        }
+
+
+
+
+        init {
+            paint.isAntiAlias = true
+            paint.color = Color.BLACK
+            paint.style = Paint.Style.STROKE
+            paint.strokeJoin = Paint.Join.ROUND
+            paint.strokeWidth = CrearValeEmpresaActivity.STROKE_WIDTH
+        }
     }
 }
 
