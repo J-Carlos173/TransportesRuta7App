13f715adc7c22deb210f6047d86986081e7c0e7f
Carlos <krlos173173@gmail.com>
2022-05-23 18:37:04 -0400

    exportar vales actvity

--- TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/ValesActivity.kt
+++ TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/ValesActivity.kt
@@ -2,10 +2,26 @@ package com.mana.transportesruta7app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_vales.*
 
 class ValesActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vales)
+
+        setup()
     }
+    private fun setup() {
+        exportarButton.setOnClickListener(){
+
+
+
+        }
+    }
+
 }
