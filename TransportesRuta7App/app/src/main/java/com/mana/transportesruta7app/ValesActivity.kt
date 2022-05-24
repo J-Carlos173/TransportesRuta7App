e026ca8fa052b29cd1943ac65c3994c6f2834487
Isa <isaiasa42@gmail.com>
2022-05-23 20:54:07 -0400

    Actualizacion

--- TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/ValesActivity.kt
+++ TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/ValesActivity.kt
@@ -1,12 +1,21 @@
 package com.mana.transportesruta7app
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.os.FileUtils
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_vales.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.File
 
 class ValesActivity : AppCompatActivity() {
+
+
 
     val db = Firebase.firestore
 
@@ -18,10 +27,28 @@ class ValesActivity : AppCompatActivity() {
     }
     private fun setup() {
         exportarButton.setOnClickListener(){
+            val list : MutableList<String> = ArrayList()
+            val jsonArray: JSONArray = JSONArray()
+            db.collection("Vales").get().addOnSuccessListener { documents ->
+                for (document in documents) {
+                    list.add(document.data.toString())
+                }
+                val jsArray = JSONArray(list)
+                println(jsArray)
 
+
+
+
+            }
+                .addOnFailureListener { exception ->
+                    Log.w("TAG", "Error getting documents: ", exception)
+                }
 
 
         }
     }
 
+
+
 }
+
