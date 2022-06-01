2837ad295aa3ffcce8a80b7bba067a4662d681ec
javieeercl <44103734+javieeercl@users.noreply.github.com>
2022-06-01 00:32:43 -0400

    Splashscreen actualizado
    Pre-Lanzamiento

--- TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/SplashScreen.kt
+++ TransportesRuta7App/app/src/main/java/com/mana/transportesruta7app/SplashScreen.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("DEPRECATION")
@@ -23,9 +24,16 @@ class SplashScreen : AppCompatActivity() {
 
         // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
         // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
-        val backgroundImage: ImageView = findViewById(R.id.SplashScreenImage)
-        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
-        backgroundImage.startAnimation(slideAnimation)
+        val logoRuta7ImageView: ImageView = findViewById(R.id.logoRuta7imageView)
+        val transporteTextView: TextView = findViewById(R.id.transporteTextView)
+        val ruta7TextView: TextView = findViewById(R.id.Ruta7textView)
+
+        val animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba)
+        val animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo)
+
+        transporteTextView.startAnimation(animacion2)
+        ruta7TextView.startAnimation(animacion2)
+        logoRuta7ImageView.startAnimation(animacion1)
 
         // we used the postDelayed(Runnable, time) method
         // to send a message with a delayed time.
